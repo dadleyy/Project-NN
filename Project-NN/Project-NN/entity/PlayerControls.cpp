fd958416091d5b20eb28edc0f626fd1c4bc4ade1
unknown <cah2792@NM2550-17.main.ad.rit.edu>
2013-04-14 16:00:27 -0400

    Updated the player controls to move the ship instead of the camera

--- Project-NN/Project-NN/entity/PlayerControls.cpp
+++ Project-NN/Project-NN/entity/PlayerControls.cpp
@@ -47,20 +47,33 @@ void PlayerControls::handleKey( int keycode, float dt )
     switch(keycode)
     {
     case 'A':
-        drawAtts->camera.Strafe( -CAMERA_VELOCITY * dt );
-        break;
+        //drawAtts->camera.Strafe( -CAMERA_VELOCITY * dt );
+		physics->setAcceleration( XMFLOAT3(physics->acceleration.x, physics->acceleration.y - .2, physics->acceleration.z));
+		break;
     case 'D':
-        drawAtts->camera.Strafe( CAMERA_VELOCITY * dt );
+        //drawAtts->camera.Strafe( CAMERA_VELOCITY * dt );
+		physics->setAcceleration( XMFLOAT3(physics->acceleration.x, physics->acceleration.y + .2, physics->acceleration.z));
         break;
     case 'S':
-        drawAtts->camera.Walk( -CAMERA_VELOCITY * dt );
+        //drawAtts->camera.Walk( -CAMERA_VELOCITY * dt );
+		physics->setAcceleration( XMFLOAT3(physics->acceleration.x - .2, physics->acceleration.y, physics->acceleration.z) );
         break;
     case 'W':
-        drawAtts->camera.Walk( CAMERA_VELOCITY * dt );
+        //drawAtts->camera.Walk( CAMERA_VELOCITY * dt );
+		physics->setAcceleration( XMFLOAT3(physics->acceleration.x + .2, physics->acceleration.y, physics->acceleration.z) );
         break;
-	case 'X':
-		physics->setAcceleration( XMFLOAT3(physics->acceleration.x + .2, 0, 0) );
-
+	case 'Q':
+		drawAtts->camera.Walk( CAMERA_VELOCITY * dt );
+		break;
+	case 'E':
+		drawAtts->camera.Walk( -CAMERA_VELOCITY * dt );
+		break;
+	case 'Z':
+		drawAtts->camera.Strafe( -CAMERA_VELOCITY * dt );
+		break;
+	case 'C':
+		drawAtts->camera.Strafe( CAMERA_VELOCITY * dt );
+		break;
     default:
         break;
     }
