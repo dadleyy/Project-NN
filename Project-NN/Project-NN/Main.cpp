9a6d72e99d5f91398342f9c47be5e9ec8b850922
Dennis Honeyman <arcticuno@gmail.com>
2013-04-15 15:30:15 -0400

    Fix formatting in Main.cpp.

--- Project-NN/Project-NN/Main.cpp
+++ Project-NN/Project-NN/Main.cpp
@@ -20,8 +20,7 @@ int screenWidth;
 int screenHeight;
 void addResources();
 
-class Game : public D3DApp
-{
+class Game : public D3DApp {
 public:
 	Game(HINSTANCE hInstance);
 	~Game();
@@ -29,17 +28,17 @@ public:
 	bool Init();
 	void OnResize();
 	void UpdateScene(float dt);
-	void DrawScene(); 
+	void DrawScene();
 
 	void OnMouseDownL(WPARAM btnState, int x, int y);
 	void OnMouseDownM(WPARAM btnState, int x, int y);
 	void OnMouseDownR(WPARAM btnState, int x, int y);
-    void OnMouseUpL(WPARAM btnState, int x, int y);
+	void OnMouseUpL(WPARAM btnState, int x, int y);
 	void OnMouseUpM(WPARAM btnState, int x, int y);
 	void OnMouseUpR(WPARAM btnState, int x, int y);
-    void OnMouseMove(WPARAM btnState, int x, int y);
-    void OnKeyDown(WPARAM keyCode);
-    void OnKeyUp(WPARAM keyCode);
+	void OnMouseMove(WPARAM btnState, int x, int y);
+	void OnKeyDown(WPARAM keyCode);
+	void OnKeyUp(WPARAM keyCode);
 
 
 
@@ -51,26 +50,24 @@ private:
 };
 
 
-void ShowConsoleWindow()
-{
+void ShowConsoleWindow() {
 	AllocConsole();
 
-    HANDLE handle_out = GetStdHandle(STD_OUTPUT_HANDLE);
-    int hCrt = _open_osfhandle((long) handle_out, _O_TEXT);
-    FILE* hf_out = _fdopen(hCrt, "w");
-    setvbuf(hf_out, NULL, _IONBF, 1);
-    *stdout = *hf_out;
+	HANDLE handle_out = GetStdHandle(STD_OUTPUT_HANDLE);
+	int hCrt = _open_osfhandle((long) handle_out, _O_TEXT);
+	FILE* hf_out = _fdopen(hCrt, "w");
+	setvbuf(hf_out, NULL, _IONBF, 1);
+	*stdout = *hf_out;
 
-    HANDLE handle_in = GetStdHandle(STD_INPUT_HANDLE);
-    hCrt = _open_osfhandle((long) handle_in, _O_TEXT);
-    FILE* hf_in = _fdopen(hCrt, "r");
-    setvbuf(hf_in, NULL, _IONBF, 128);
-    *stdin = *hf_in;
+	HANDLE handle_in = GetStdHandle(STD_INPUT_HANDLE);
+	hCrt = _open_osfhandle((long) handle_in, _O_TEXT);
+	FILE* hf_in = _fdopen(hCrt, "r");
+	setvbuf(hf_in, NULL, _IONBF, 128);
+	*stdin = *hf_in;
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
-				   PSTR cmdLine, int showCmd)
-{
+                   PSTR cmdLine, int showCmd) {
 	// Enable run-time memory check for debug builds.
 #if defined(DEBUG) | defined(_DEBUG)
 	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
@@ -79,25 +76,22 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
 #endif
 
 	Game app(hInstance);
-	
+
 	if(!app.Init())
 		return 0;
-	
+
 	return app.Run();
 }
- 
 
-Game::Game(HINSTANCE hInstance) : D3DApp(hInstance), manager()
-{
+
+Game::Game(HINSTANCE hInstance) : D3DApp(hInstance), manager() {
 	mMainWndCaption = L"Project NN";
 }
 
-Game::~Game()
-{
+Game::~Game() {
 }
 
-bool Game::Init()
-{
+bool Game::Init() {
 	if(!D3DApp::Init())
 		return false;
 
@@ -121,27 +115,24 @@ bool Game::Init()
 	return true;
 }
 
-void Game::OnResize()
-{
+void Game::OnResize() {
 	D3DApp::OnResize();
-    screenWidth = mClientWidth;
-    screenHeight = mClientHeight;
+	screenWidth = mClientWidth;
+	screenHeight = mClientHeight;
 	if(resourceMgr != nullptr)
 		resourceMgr->camera.SetLens(resourceMgr->camera.GetFovY(),
-			((float)screenWidth)/screenHeight,
-			resourceMgr->camera.GetNearZ(),
-			resourceMgr->camera.GetFarZ());
+		                            ((float)screenWidth)/screenHeight,
+		                            resourceMgr->camera.GetNearZ(),
+		                            resourceMgr->camera.GetFarZ());
 }
 
-void Game::UpdateScene(float dt)
-{
+void Game::UpdateScene(float dt) {
 	//TODO: Commented because of performance issues.
 	//physicsMgr->CheckForCollisions();
 	manager.Update(dt);
 }
 
-void Game::DrawScene()
-{
+void Game::DrawScene() {
 	md3dImmediateContext->ClearRenderTargetView(mRenderTargetView, reinterpret_cast<const float*>(&Colors::LightSteelBlue));
 	md3dImmediateContext->ClearDepthStencilView(mDepthStencilView, D3D11_CLEAR_DEPTH|D3D11_CLEAR_STENCIL, 1.0f, 0);
 
@@ -151,54 +142,44 @@ void Game::DrawScene()
 }
 
 
-void Game::OnMouseDownL(WPARAM btnState, int x, int y)
-{
+void Game::OnMouseDownL(WPARAM btnState, int x, int y) {
 	input->OnMouseDown(btnState, L_btn, x, y);
 }
 
-void Game::OnMouseDownM(WPARAM btnState, int x, int y)
-{
+void Game::OnMouseDownM(WPARAM btnState, int x, int y) {
 	input->OnMouseDown(btnState, M_btn, x, y);
 }
 
-void Game::OnMouseDownR(WPARAM btnState, int x, int y)
-{
+void Game::OnMouseDownR(WPARAM btnState, int x, int y) {
 	input->OnMouseDown(btnState, R_btn, x, y);
 }
 
-void Game::OnMouseUpL(WPARAM btnState, int x, int y)
-{
-    input->OnMouseUp(btnState, L_btn, x, y);
+void Game::OnMouseUpL(WPARAM btnState, int x, int y) {
+	input->OnMouseUp(btnState, L_btn, x, y);
 }
 
-void Game::OnMouseUpM(WPARAM btnState, int x, int y)
-{
-    input->OnMouseUp(btnState, M_btn, x, y);
+void Game::OnMouseUpM(WPARAM btnState, int x, int y) {
+	input->OnMouseUp(btnState, M_btn, x, y);
 }
 
-void Game::OnMouseUpR(WPARAM btnState, int x, int y)
-{
-    input->OnMouseUp(btnState, R_btn, x, y);
+void Game::OnMouseUpR(WPARAM btnState, int x, int y) {
+	input->OnMouseUp(btnState, R_btn, x, y);
 }
 
 
 
-void Game::OnMouseMove(WPARAM btnState, int x, int y)
-{
-    input->OnMouseMove(btnState, x, y);
+void Game::OnMouseMove(WPARAM btnState, int x, int y) {
+	input->OnMouseMove(btnState, x, y);
 }
 
-void Game::OnKeyDown(WPARAM keyCode)
-{
-    input->OnKeyDown(keyCode);
+void Game::OnKeyDown(WPARAM keyCode) {
+	input->OnKeyDown(keyCode);
 }
-void Game::OnKeyUp(WPARAM keyCode)
-{
-    input->OnKeyUp(keyCode);
+void Game::OnKeyUp(WPARAM keyCode) {
+	input->OnKeyUp(keyCode);
 }
 
-void addResources()
-{
+void addResources() {
 	//constant buffers
 	resourceMgr->addCBuffer(816, "Light");
 	resourceMgr->addCBuffer(144, "Camera");
