85c4445b05668fbb6a7a268f7af8182cb5b00918
Dennis Honeyman <arcticuno@gmail.com>
2013-05-08 14:39:01 -0400

    Add basic update to Main Menu.

--- Project-NN/states/MainMenu.cpp
+++ Project-NN/states/MainMenu.cpp
@@ -1,5 +1,8 @@
 #include "MainMenu.h"
 
+#include "entity/GameObject.h"
+#include "SceneManager.h"
+
 MainMenu MainMenu::instance;
 
 void MainMenu::Init(StateManager* manager) {
@@ -9,6 +12,11 @@ void MainMenu::Cleanup() {
 }
 
 void MainMenu::Update(float dt) {
+	for(auto it = sceneMgr->Begin(); it != sceneMgr->End(); ++it) {
+		if(!(*it)->active)
+			continue;
+		(*it)->Update(dt);
+	}
 }
 
 void MainMenu::Draw() {
